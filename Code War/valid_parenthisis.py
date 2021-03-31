905797019669b4ebc14fabfab5df2b9a72d0d11f
Anish Shrestha <voidarchived@gmail.com>
2021-03-31 17:37:55 +0545

    udpate valid parenthisis

--- Code War/valid_parenthisis.py
+++ Code War/valid_parenthisis.py
@@ -1,5 +1,5 @@
 # Write a function that takes a string of parentheses,
-# and determines if the order of the parentheses is valid. 
+# and determines if the order of the parentheses is valid.
 # The function should return true if the string is valid, and false if it's invalid.
 
 # Examples
@@ -20,12 +20,22 @@ def valid_parentheses(string):
                 stack.pop()
             except:
                 return False
-    
+
     if len(stack) == 0:
         return True
     else:
         return False
 
+# best solution
 
 
-
+def valid_parentheses_2(string):
+    cnt = 0
+    for char in string:
+        if char == '(':
+            cnt += 1
+        if char == ')':
+            cnt -= 1
+        if cnt < 0:
+            return False
+    return True if cnt == 0 else False
