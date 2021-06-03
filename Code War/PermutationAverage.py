59d5bcf7f75289be2dba96899c5a1d29c399f837
Anish Shrestha <voidarchived@gmail.com>
2021-06-03 22:51:22 +0545

    permutation

---
+++ Code War/PermutationAverage.py
@@ -0,0 +1,11 @@
+
+def permutation_average(n):
+    n = str(n)
+    n, l = sum(map(int, n)) / float(len(n)), len(n)
+
+    n = n * ((10 ** l - 1) / 9)
+
+    return round(n)
+
+
+print(permutation_average(24))
